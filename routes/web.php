1fa5b0c4964a59b0dc7b6dd4fee55a6e5f19a8c8
nechasyifa <nechasyifasyafitri@gmail.com>
2025-02-22 02:21:30 +0700

    Controller Jobsheet2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController; //menggunakan ItemController untuk menangani permintaan terkait item
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,29 +20,37 @@ use App\Http\Controllers\ItemController; //menggunakan ItemController untuk mena
 |
 */
 
-Route::get('/', function () {
-    return view('welcome'); //menampilkan halaman welcome.blade.php saat pengguna mengakses URL utama "/"
-});
+// Route::get('/', function () {
+//     return view('welcome'); //menampilkan halaman welcome.blade.php saat pengguna mengakses URL utama "/"
+// });
 
 Route::resource('items', ItemController::class); //membuat rute CRUD otomatis untuk resource 'items'
 
-Route::get('/hello', function () {
-    return 'Hello World';
-}); //menampilkan "Hello World" ketika diakses melalui URL "/hello"
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+// Route::get('/', [PageController::class,'index']);
+// Route::get('/about', [PageController::class,'about']);
+// Route::get('/articles/{id}', [PageController::class,'articles']);
+
+Route::get('/', HomeController::class);
+Route::get('/about', AboutController::class);
+Route::get('/articles/{id}', ArticleController::class);
+
+Route::resource('photos', PhotoController::class);
 
 Route::get('/world', function () {
     return 'World';
 }); //menampilkan "World" ketika diakses melalui URL "/world"
 
-Route::get('/', function () {
-    return ('Selamat Datang');
-}); //menampilkan halaman "Selamat Datang" saat pengguna mengakses URL utama "/"
+// Route::get('/', function () {
+//     return ('Selamat Datang');
+// }); //menampilkan halaman "Selamat Datang" saat pengguna mengakses URL utama "/"
 
-Route::get('/about', function () {
-    return ("NIM: 2341720167 <br> Nama: Necha Syifa Syafitri");
-}); //menampilkan halaman "NIM: 2341720167 dan Nama: Necha Syifa Syafitri" saat pengguna mengakses URL "/about"
+// Route::get('/about', function () {
+//     return ("NIM: 2341720167 <br> Nama: Necha Syifa Syafitri");
+// }); //menampilkan halaman "NIM: 2341720167 dan Nama: Necha Syifa Syafitri" saat pengguna mengakses URL "/about"
 
-Route::get('/user/{Necha}', function ($name) {
+Route::get('/user/{name}', function ($name) {
     return 'Nama saya ' . $name;
 });
 
@@ -44,9 +58,9 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
 
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID ' . $id;
-});
+// Route::get('/articles/{id}', function ($id) {
+//     return 'Halaman Artikel dengan ID ' . $id;
+// });
 
 // Route::get('/user/{name?}', function ($name=null) {
 //     return 'Nama saya '.$name;
